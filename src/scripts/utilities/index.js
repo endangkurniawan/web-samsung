f593826b9c402280bae07457a7cf5e5321450e90
endang_kurniawan <endangkurniawan130@gmail.com>
2024-06-26 11:11:43 +0700

    firt commit syncup

---
+++ src/scripts/utilities/index.js
@@ -0,0 +1,18 @@
+/* ------------------------------------------------------------------------------
+@name: Utilities Index
+@description: Indexing all utilities
+--------------------------------------------------------------------------------- */
+
+import isOS from './isOS';
+import BrowserCheck from './BrowserCheck';
+import Scrolllable from './Scrolllable';
+import Validation from './Validation';
+import Session from './Session';
+
+export {
+  isOS,
+  BrowserCheck,
+  Scrolllable,
+  Validation,
+  Session
+};
