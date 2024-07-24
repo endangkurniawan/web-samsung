91ddb31780fa9bca579a8a0cbab90ab4394e8e6d
endang_kurniawan <endangkurniawan130@gmail.com>
2024-07-24 11:16:48 +0700

    update

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -1,12 +1,46 @@
 /* ------------------------------------------------------------------------------
 @name: Footer
 @description: Footer
---------------------------------------------------------------------------------- */
+----------------------------
+----------------------------------------------------- */
 
 const Footer = (() => {
-  // hanlePaddingBottomMain
+  // handleAccordion
+  const handleAccordion = () => {
+    $(".js-footer-accordion .footer__nav__title").on("click", (e) => {
+      if ($(window).width() <= 575.8) {
+        const _this = $(e.currentTarget);
+        const _navBox = _this.parents(".footer__nav__box");
+
+        // Tutup semua menu lainnya
+        $(".js-footer-accordion .footer__nav__box")
+          .not(_navBox)
+          .find(".footer__menu")
+          .slideUp(300);
+        $(".js-footer-accordion .footer__nav__box")
+          .not(_navBox)
+          .removeClass("footer__nav--active");
+        $(".js-footer-accordion .footer__nav__title")
+          .not(_this)
+          .removeClass("active");
+
+        // Buka atau tutup menu yang diklik
+        if (_this.hasClass("active")) {
+          _navBox.find(".footer__menu").slideUp(300);
+          _navBox.removeClass("footer__nav--active");
+          _this.removeClass("active");
+        } else {
+          _navBox.find(".footer__menu").slideDown(300);
+          _navBox.addClass("footer__nav--active");
+          _this.addClass("active");
+        }
+      }
+    });
+  };
+
+  // handlePaddingBottomMain
   const handlePaddingBottomMain = () => {
-    if ($(window).width() > 575.5) {
+    if ($(window).width() > 767.96) {
       const _heightFooter = $(".footer").innerHeight();
       $(".js-main-site").css("padding-bottom", `${_heightFooter}px`);
     } else {
@@ -18,6 +52,7 @@ const Footer = (() => {
   const init = () => {
     $(document).ready(() => {
       if ($(".footer").length) {
+        handleAccordion();
         handlePaddingBottomMain();
       }
     });
